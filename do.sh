d1fd337a0188e9ec3aa12747366131c3ac5ebf25
Sumit Chohan <sumitchohan@Sumits-MacBook-Pro.local>
2019-01-11 11:07:10 +0530

    wip

--- do.sh
+++ do.sh
@@ -744,11 +744,18 @@ Init()
 	input keyevent 3 
 
 }
-
-
+isTroopPresent="y"
 Touch()
 {
-    dd if=/sdcard/coc/gestures/tap_$1.rec of=/dev/input/event3 2>/sdcard/results.txt
+	if [ "$isTroopPresent" = "y" ]
+	then
+    	dd if=/sdcard/coc/gestures/tap_$1.rec of=/dev/input/event3 2>/sdcard/results.txt
+	fi
+}
+
+SelectTroop()
+{
+	isTroopPresent="y"
 }
 
 DeployTL()
@@ -853,6 +860,8 @@ Start()
 	echo "kill $tid">>thread_info
 }
 
+
+
 LogRemote()
 { 
 	dt=$(date '%H_%M_%S');
@@ -860,4 +869,20 @@ LogRemote()
 	dd if=log_remote of=log_remote_head ibs=1 skip=0 count=1000 2>/sdcard/results.txt
 	cp log_remote_head log_remote
 	curl -d "$(cat log_remote)" -X POST https://api.keyvalue.xyz/bc4b42e6/logKey -k -s
+}
+
+Choose()
+{
+    if [ "$1" = "$2" ]
+    then 
+        Tap $3 $4
+        isTroopPresent="y"
+    fi
+}
+
+MapTroops()
+{ 
+    rm /sdcard/coc/intent_completed
+    am startservice -n com.example.akshika.opencvtest/.MyIntentService --es screenImgPath scr.png --es itemImgPaths king,primo,queen,archer --es directoryPath /sdcard/coc --es resultsFilePath choose.sh --es input_offset_x 0 --es input_offset_y 0 --es input_width 125 --es input_height 1200
+    WaitForFile /sdcard/coc/intent_completed
 }
--- temp.sh
+++ temp.sh
@@ -10,3 +10,43 @@ do
     sleep 1
 done
 }
+
+Start()
+{
+	echo "">> thread_info
+	source thread_info
+	echo ""> thread_info
+	StartThread &
+	tid=$!
+	echo "kill $tid">>thread_info
+}
+
+Tap()
+{
+    echo "x - $1   y - $2"
+}
+
+LogRemote()
+{ 
+	dt=$(date '%H_%M_%S');
+	echo "$dt - $1<br>$(cat log_remote)">log_remote
+	dd if=log_remote of=log_remote_head ibs=1 skip=0 count=1000 2>/sdcard/results.txt
+	cp log_remote_head log_remote
+	curl -d "$(cat log_remote)" -X POST https://api.keyvalue.xyz/bc4b42e6/logKey -k -s
+}
+
+Choose()
+{
+    if [ "$1" = "$2" ]
+    then 
+        Tap $3 $4
+        isTroopPresent="y"
+    fi
+}
+
+MapTroops()
+{ 
+    rm /sdcard/coc/intent_completed
+    am startservice -n com.example.akshika.opencvtest/.MyIntentService --es screenImgPath scr.png --es itemImgPaths king,primo,queen,archer --es directoryPath /sdcard/coc --es resultsFilePath choose.sh --es input_offset_x 0 --es input_offset_y 0 --es input_width 125 --es input_height 1200
+    WaitForFile /sdcard/coc/intent_completed
+}
