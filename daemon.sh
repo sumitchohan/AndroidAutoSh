1425d1733c68ab3ee6c51699f1d379fab1e0b8bd
Sumit Chohan <sumit@Sumits-MacBook-Pro.local>
2019-12-07 22:43:41 -0600

    wip

--- daemon.sh
+++ daemon.sh
@@ -2,6 +2,6 @@ heartBeatDelay=60
 while [ 1 -le 2 ]
 do
     curl -s -k https://raw.githubusercontent.com/sumitchohan/AndroidAutoSh/master/ping.sh -o ping.sh 
-    source ping.sh
+    source ping.sh &
 	sleep $heartBeatDelay
 done
